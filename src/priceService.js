f75a89e46970f5266a813a03db73c909bf5a0e1d
unknown <sanjibansh2012@yahoo.com>
2024-03-24 00:29:11 +0530

    pool removed for rener server test

--- src/priceService.js
+++ src/priceService.js
@@ -11,7 +11,7 @@ class PriceService {
       });
 
       // Fetch pricing details from the database
-      return 123;
+      return pool;
       const query = `
     SELECT base_distance_in_km, km_price, fix_price
     FROM Pricing
