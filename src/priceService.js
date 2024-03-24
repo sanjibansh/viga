a4f42df1a6174478ffa7657e3b81e3faf578a6e7
unknown <sanjibansh2012@yahoo.com>
2024-03-24 15:49:35 +0530

    pool removed for rener server test

--- src/priceService.js
+++ src/priceService.js
@@ -1,61 +1,7 @@
-// const { Pool } = require('pg');
-// //const connectionString = require('./models/models');
-// const connectionString = 'postgres://postgres:Password%4003@localhost:5432/postgres';
-
-
-// class PriceService {
-//   async calculatePrice(zone, organization_id, total_distance, type) {
-//     try {
-//       const pool = new Pool({
-//         connectionString: connectionString,
-//       });
-
-//       // Fetch pricing details from the database
-     
-//       const query = `
-//     SELECT base_distance_in_km, km_price, fix_price
-//     FROM Pricing
-//     WHERE organization_id = $1 AND zone = $2 AND item_id IN (
-//       SELECT id FROM Item WHERE type = $3
-//     )
-//   `;
-//   // return [zone, organization_id, total_distance, type]
-   
-//       const { rows } = await pool.query(query, [organization_id, zone, type]);
-//       //error in this above line
-//       return 122;
-//       if (rows.length === 0) {
-//         return {
-//           error: "Pricing information not found for the given parameters",
-//         }
-//       }
-//       return rows[0];
-
-//       //let distance = Integer.parseInt(distance);
-
-//       // Calculate total price
-//       const { base_distance_in_km, km_price, fix_price } = rows[0];
-//       let totalPrice = parseInt(fix_price) * 100;
-//       if (total_distance > base_distance_in_km) {
-//         totalPrice += (total_distance - base_distance_in_km) * km_price * 100; // Convert to cents
-//       }
-
-//       // Return the total price in euros
-//       return rows[0] ;//totalPrice / 100 ; // Convert back to euros
-//     } catch (error) {
-//       console.error("Error calculating price:", error);
-//       return { error: "Internal server error"};
-//     }
-//   }
-// }
-
-// module.exports = PriceService;
-
-
-
 const { Pool } = require('pg');
 //const connectionString = require('./models/models');
-const connectionString = 'postgres://postgres:Password%4003@localhost:5432/postgres';
+//const connectionString = 'postgres://postgres:Password%4003@localhost:5432/postgres';
+const connectionString = 'postgres://user_viga:h8CueJ9erUc4Nkj82RYiHUUUUo986Y2i@dpg-cnvvkpv79t8c73d862rg-a/postgres_viga'
 
 class PriceService {
   async calculatePrice(zone, organization_id, total_distance, type) {
@@ -64,43 +10,39 @@ class PriceService {
         connectionString: connectionString,
       });
 
-      // Fetch pricing details from the database
+      // Fetch pricing details from the database  
       const query = `
-        SELECT base_distance_in_km, km_price, fix_price
-        FROM Pricing
-        WHERE organization_id = $1 AND zone = $2 AND item_id IN (
-          SELECT id FROM Item WHERE type = $3
-        )
-      `;
-      
-      console.log('Executing SQL query:', query);
-      console.log('Parameters:', [organization_id, zone, type]);
-
+    SELECT base_distance_in_km, km_price, fix_price
+    FROM Pricing
+    WHERE organization_id = $1 AND zone = $2 AND item_id IN (
+      SELECT id FROM Item WHERE type = $3
+    )
+  `;
+  console.log('Executing SQL query:', query);
+  console.log('Parameters:', [organization_id, zone, type]);
+ 
       const { rows } = await pool.query(query, [organization_id, zone, type]);
-      
-      console.log('Query result:', rows);
-
+      //error in this above line
       if (rows.length === 0) {
         return {
           error: "Pricing information not found for the given parameters",
-        };
+        }
       }
 
-      // Calculate total price (if needed)
-      //       // Calculate total price
+      // Calculate total price
       const { base_distance_in_km, km_price, fix_price } = rows[0];
       let totalPrice = parseInt(fix_price) * 100;
       if (total_distance > base_distance_in_km) {
         totalPrice += (total_distance - base_distance_in_km) * km_price * 100; // Convert to cents
       }
-      console.log("final price: ",totalPrice)
-      return [totalPrice, rows[0]];
+
+      // Return the total price in euros
+      return totalPrice / 100 ; // Convert back to euros
     } catch (error) {
-      console.error("Error executing SQL query:", error);
-      return { error: "Internal server error" };
+      console.error("Error calculating price:", error);
+      return { error: "Internal server error"};
     }
   }
 }
 
 module.exports = PriceService;
-
