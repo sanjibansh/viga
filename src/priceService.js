42b7b8d9dee396382c496d9aca3934a8284cca85
unknown <sanjibansh2012@yahoo.com>
2024-04-05 15:59:02 +0530

    Revert "Revert "swaggar added""
    This reverts commit 1c793ca94b1af4aea76d60770cb1880187dc9671.

--- src/priceService.js
+++ src/priceService.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 //const connectionString = require('./models/models');
-//const connectionString = 'postgres://postgres:Password%4003@localhost:5432/postgres';
-const connectionString = 'postgres://user_viga:h8CueJ9erUc4Nkj82RYiHUUUUo986Y2i@dpg-cnvvkpv79t8c73d862rg-a/postgres_viga'
+const connectionString = 'postgres://postgres:Password%4003@localhost:5432/postgres';
+//const connectionString = 'postgres://user_viga:h8CueJ9erUc4Nkj82RYiHUUUUo986Y2i@dpg-cnvvkpv79t8c73d862rg-a:5432/postgres_viga'
+//postgres://user_viga:h8CueJ9erUc4Nkj82RYiHUUUUo986Y2i@dpg-cnvvkpv79t8c73d862rg-a.oregon-postgres.render.com/postgres_viga
 
 class PriceService {
   async calculatePrice(zone, organization_id, total_distance, type) {
@@ -19,9 +20,10 @@ class PriceService {
     )
   `;
   console.log('Executing SQL query:', query);
-  console.log('Parameters:', [organization_id, zone, type]);
+  console.log('Parameters:', [organization_id,total_distance, zone, type]);
  
       const { rows } = await pool.query(query, [organization_id, zone, type]);
+      console.log(rows[0])
       //error in this above line
       if (rows.length === 0) {
         return {
@@ -31,10 +33,16 @@ class PriceService {
 
       // Calculate total price
       const { base_distance_in_km, km_price, fix_price } = rows[0];
+      let base_distance_in_km1 = parseInt(base_distance_in_km);
+      let km_price1 = parseFloat(km_price);
+      let total_distance1 = parseInt(total_distance)
+
       let totalPrice = parseInt(fix_price) * 100;
-      if (total_distance > base_distance_in_km) {
+      if (total_distance1 > base_distance_in_km) {
         totalPrice += (total_distance - base_distance_in_km) * km_price * 100; // Convert to cents
       }
+      console.log('Parametersssssssssss:', [organization_id,total_distance1, zone, type, km_price1]);
+
 
       // Return the total price in euros
       return totalPrice / 100 ; // Convert back to euros
